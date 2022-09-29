ba5dbcc869073fc19b812114010958481118da8f
Huawei <Huawei@LAPTOP-5LRHA01L>
2022-09-29 14:36:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double a;
+        Console.WriteLine("Введите значение a:");
+        a = int.Parse(Console.ReadLine());
+        if (-15 < a && a<= 12) 
+            {
+            Console.WriteLine("True");
+            }
+        else if (14< a&& a<17)
+        {
+            Console.WriteLine("True");
+        }
+        else if (19 <= a)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+}
